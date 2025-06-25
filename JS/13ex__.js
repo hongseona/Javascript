cde2f81032cde5e27c443d9651b5250b707dd81d
Hongseona <ghdtjsdk312@gmail.com>
2025-06-25 13:33:48 +0900

    js day4

---
+++ JS/13ex__.js
@@ -0,0 +1,30 @@
+//함수 정의 - 함수를 변수에 할당하는 방식은 '끌어올리기(호이스팅)' 못함
+function add(){
+    console.log(100+1000)
+}
+function sub(){
+    return 100-1000;    //함수
+}
+function muliply(num1,num2){ 
+    console.log('num1*num2 :', num1*num2)
+}
+function div(num1,num2){
+    return num1/num2
+}
+
+add()
+add()
+add()
+
+let result = sub()      //함수를 호출하면 결과값을 전달 받습니다.
+console.log('뺄셈', result)
+console.log('뺄셈', sub())
+
+muliply(23,56)  //1288  //23,56 은 함수 실행에 필요한 입력값
+muliply(111,23)  //2553
+
+result = div (77,13)
+console.log('나누기:', result)
+console.log('나누기:', div(77,13))
+console.log('나눈 결과 소수점:', result.toFixed(3))
+
